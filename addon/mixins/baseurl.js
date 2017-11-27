908334dc5a4b2b1f0bdcdb5f6a58c70dbb69abf7
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-11-27 17:04:28 +0400

    5

--- addon/mixins/baseurl.js
+++ addon/mixins/baseurl.js
@@ -3,8 +3,12 @@ import config from 'ember-get-config';
 
 export default Ember.Mixin.create({
 
+    location: Ember.inject.service(),
+
     baseURL: Ember.computed(function() {
-		return config.APP.protocol + 's3-'+config.S3.region + '.amazonaws.com/' + config.S3.bucket + '/';
+
+		return this.get('location.protocol') + 's3-'+config.S3.region + '.amazonaws.com/' + config.S3.bucket + '/';
+
 	}),
 
 });
