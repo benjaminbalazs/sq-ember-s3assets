46d0cbd0b05517fcc472ba2a9b9cd81615ee4927
Benjamin Balazs <benjaminbalazs@gmail.com>
2018-05-20 12:09:31 +0400

    state

--- addon/models/video.js
+++ addon/models/video.js
@@ -5,15 +5,15 @@ import config from 'ember-get-config';
 
 export default File.extend({
 
+	state: attr('string'),
+
+	completed_at: attr('date'),
+
 	width: attr('number'),
 
 	height: attr('number'),
 
 	duration: attr('number'),
-
-	state: attr('string'),
-
-	completed_at: attr('date'),
 
 	preview: attr('string'),
 
@@ -26,5 +26,50 @@ export default File.extend({
 	autoplay: attr('boolean'),
 
 	showtrailer: attr('boolean'),
+
+	//
+
+	videos: computed('files', function() {
+
+        return this.get('files').filter(function(item) {
+
+            return ( item.indexOf('/mp4/') !== -1 || item.indexOf('/hls/') !== -1 || item.indexOf('/webm/') !== -1 );
+
+        }).map(function(item) {
+
+			let type;
+
+			if ( item.indexOf('/mp4/') !== -1 ) {
+
+				type = "video/mp4";
+
+			} else if ( item.indexOf('/webm/') !== -1 ) {
+
+				type = "video/webm";
+
+			} else if ( item.indexOf('/hls/') !== -1 ) {
+
+				type = "application/x-mpegURL";
+
+			}
+
+			return {
+				src: item,
+				type: type,
+			}
+
+		});
+
+    }),
+
+    thumbnails: computed('files', function() {
+
+        return this.get('files').filter(function(item) {
+
+            return ( item.indexOf('/thumbs/') !== -1 );
+
+        });
+
+    }),
 
 });
